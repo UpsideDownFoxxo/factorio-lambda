09c3107ff132945165ee858263f02828b704eff3
Fox <No>
2025-08-24 02:50:02 +0200

    Add swap utility function

--- lib/utils.lua
+++ lib/utils.lua
@@ -1,11 +1,30 @@
 local m = {}
 
+---Turns a `LuaGuiElement` into an unique identifier
 ---@param el LuaGuiElement
 ---@return string
 m.get_ui_ident = function(el)
 	return tostring(el.player_index) .. "|" .. tostring(el.index)
 end
 
+----------------------
+-- Table Operations --
+----------------------
+
+---Swaps elements at the two keys
+---@generic A
+---@param t table<A,any>
+---@param a A
+---@param b A
+m.swap = function(t, a, b)
+	local tmp = t[a]
+	t[a] = t[b]
+	t[b] = tmp
+end
+
+---Creates a shallow copy of the provided table
+---@param t table
+---@return table
 m.shallow_copy = function(t)
 	if type(t) == "table" then
 		local c = {}
